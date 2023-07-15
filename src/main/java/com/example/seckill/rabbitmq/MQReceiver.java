09c3320f050dcec07c12ed8c87266c3d7a4fb5c5
HUANG Kaining <1155141441@link.cuhk.edu.hk>
2023-07-15 15:46:13 +0800

    P51 end

--- src/main/java/com/example/seckill/rabbitmq/MQReceiver.java
+++ src/main/java/com/example/seckill/rabbitmq/MQReceiver.java
@@ -1,8 +1,11 @@
 package com.example.seckill.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 @Slf4j
@@ -21,5 +24,37 @@ public class MQReceiver {
     @RabbitListener(queues = "queue_fanout02")
     public void receive02(Object msg){
         log.info("receive message from queue_fanout02: " + msg);
+    }
+
+    @RabbitListener(queues = "queue_direct01")
+    public void receive03(Object msg){
+        log.info("receive message from queue_direct01: " + msg);
+    }
+
+    @RabbitListener(queues = "queue_direct02")
+    public void receive04(Object msg){
+        log.info("receive message from queue_direct02: " + msg);
+    }
+
+    @RabbitListener(queues = "queue_topic01")
+    public void receive05(Object msg){
+        log.info("receive message from queue_topic01: " + msg);
+    }
+
+    @RabbitListener(queues = "queue_topic02")
+    public void receive06(Object msg){
+        log.info("receive message from queue_topic02: " + msg);
+    }
+
+    @RabbitListener(queues = "queue_header01")
+    public void receive07(Message msg){
+        log.info("receive message object from queue_header01: " + msg);
+        log.info("receive message from queue_header01: " + new String(msg.getBody()));
+    }
+
+    @RabbitListener(queues = "queue_header02")
+    public void receive08(Message msg){
+        log.info("receive message object from queue_header02: " + msg);
+        log.info("receive message from queue_header02: " + new String(msg.getBody()));
     }
 }
--- src/main/java/com/example/seckill/rabbitmq/MQSender.java
+++ src/main/java/com/example/seckill/rabbitmq/MQSender.java
@@ -1,6 +1,8 @@
 package com.example.seckill.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,43 @@ public class MQSender {
     public void send(Object msg){
         log.info("send message: " + msg);
         rabbitTemplate.convertAndSend("fanoutExchange", "", msg);
+    }
+
+    public void send01(Object msg){
+        log.info("send red message: " + msg);
+        rabbitTemplate.convertAndSend("directExchange", "queue.red", msg);
+    }
+
+    public void send02(Object msg){
+        log.info("send green message: " + msg);
+        rabbitTemplate.convertAndSend("directExchange", "queue.green", msg);
+    }
+
+    public void send03(Object msg){
+        log.info("send topic queue01 message: " + msg);
+        rabbitTemplate.convertAndSend("topicExchange", "queue.red.message", msg);
+    }
+
+    public void send04(Object msg){
+        log.info("send topic queue 01 & 02 message: " + msg);
+        rabbitTemplate.convertAndSend("topicExchange", "message.queue.green.abc", msg);
+    }
+
+    public void send05(String msg){
+        log.info("send headers queue 01 & 02 message: " + msg);
+        MessageProperties properties = new MessageProperties();
+        properties.setHeader("color", "red");
+        properties.setHeader("speed", "fast");
+        Message message = new Message(msg.getBytes(), properties);
+        rabbitTemplate.convertAndSend("headersExchange", "", message);
+    }
+
+    public void send06(String msg){
+        log.info("send headers queue 01 message: " + msg);
+        MessageProperties properties = new MessageProperties();
+        properties.setHeader("color", "red");
+        properties.setHeader("speed", "normal");
+        Message message = new Message(msg.getBytes(), properties);
+        rabbitTemplate.convertAndSend("headersExchange", "", message);
     }
 }
