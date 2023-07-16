8557085ece844b1420a541e8af79b01a65f7ec2d
HUANG Kaining <1155141441@link.cuhk.edu.hk>
2023-07-17 00:35:17 +0800

    P52 end

--- src/main/java/com/example/seckill/controller/SeckillController.java
+++ src/main/java/com/example/seckill/controller/SeckillController.java
@@ -15,19 +15,24 @@ import com.example.seckill.vo.RespBean;
 import com.example.seckill.vo.RespBeanEnum;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/seckill")
 @Api(value = "秒杀", tags = "秒杀")
-public class SeckillController {
+public class SeckillController implements InitializingBean {
 
     @Autowired
     private IGoodsService goodsService;
@@ -82,23 +87,55 @@ public class SeckillController {
         if(user == null){
             return RespBean.error(RespBeanEnum.SESSION_ERROR);
         }
-        GoodsVo goods = goodsService.findGoodsVoByGoodsId(goodsId);
-        if(goods.getStockCount() < 1){
-//            model.addAttribute("errmsg", RespBeanEnum.EMPTY_STOCK.getMessage());
-            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
-        }
 
-        // check whether ordered twice
-//        QueryWrapper<SeckillOrder> eq = new QueryWrapper<SeckillOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId);
-//        SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId));
-//        SeckillOrder seckillOrder = seckillOrderMapper.selectSeckillOrder(user.getId(), goodsId);
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        // check if ordered twice
         SeckillOrder seckillOrder = (SeckillOrder) redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goodsId);
         if(seckillOrder != null){
 //            model.addAttribute("errmsg", RespBeanEnum.REPEAT_ERROR.getMessage());
             return RespBean.error(RespBeanEnum.REPEAT_ERROR);
         }
+        // pre deduct the stock
+        Long stock = valueOperations.decrement("seckillGoods:" + goodsId);
+        if(stock < 0){
+            valueOperations.increment("seckillGoods:" + goodsId);
+            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+        }
+
         Order order = orderService.seckill(user, goods);
-//        Order order = orderMapper.selectOrder(user.getId(), goodsId);
         return RespBean.success(order);
+
+//        GoodsVo goods = goodsService.findGoodsVoByGoodsId(goodsId);
+//        if(goods.getStockCount() < 1){
+////            model.addAttribute("errmsg", RespBeanEnum.EMPTY_STOCK.getMessage());
+//            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+//        }
+//
+//        // check whether ordered twice
+////        QueryWrapper<SeckillOrder> eq = new QueryWrapper<SeckillOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId);
+////        SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId));
+////        SeckillOrder seckillOrder = seckillOrderMapper.selectSeckillOrder(user.getId(), goodsId);
+//        SeckillOrder seckillOrder = (SeckillOrder) redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goodsId);
+//        if(seckillOrder != null){
+////            model.addAttribute("errmsg", RespBeanEnum.REPEAT_ERROR.getMessage());
+//            return RespBean.error(RespBeanEnum.REPEAT_ERROR);
+//        }
+//        Order order = orderService.seckill(user, goods);
+////        Order order = orderMapper.selectOrder(user.getId(), goodsId);
+//        return RespBean.success(order);
+
+
+    }
+
+    // initialize the system, load the goods stock to Redis
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<GoodsVo> list = goodsService.findGoodsVo();
+        if(CollectionUtils.isEmpty(list)){
+            return;
+        }
+        list.forEach(goodsVo -> {
+            redisTemplate.opsForValue().set("seckillGoods:" + goodsVo.getId(), goodsVo.getStockCount());
+        });
     }
 }
